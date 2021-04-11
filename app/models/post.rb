6c2c3c458f4bc0402d455050e65a1931c90f10ea
Juan Pablo <juanpablogast@gmail.com>
2021-04-11 19:42:58 -0400

    add method replace_word

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
     validates :title, :image_url, :content, presence: true
+
+
+    def replace_word
+        self.content = self.content.gsub 'spoiler', 'nombre'  
 end
